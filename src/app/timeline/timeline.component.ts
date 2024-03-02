d602f18db9df3916783444ac59d8edaceb6eeffd
Rohan Ayush <rohanayush05@gmail.com>
2024-03-02 19:10:34 +0530

    previous

--- src/app/timeline/timeline.component.ts
+++ src/app/timeline/timeline.component.ts
@@ -14,6 +14,8 @@ export class TimelineComponent {
   lenEvents: number = 0;
   counter: number = 0;
   timer: any;
+  lineColor: string = 'grey'; 
+  blueDotIndex: number | null = null;
 
   formatLabel(value: number): string {
     console.log('values', value);
@@ -47,35 +49,19 @@ export class TimelineComponent {
       // You can perform further actions based on the selectedEventId
     });
   }
-
-  ngOnInit(): void {
-    this.counter = Number(localStorage.getItem('counter'));
-
-    this.eventService.getEvents().subscribe((data: any) => {
-      this.events = data;
-    });
-    this.timer = setInterval(() => {
-      this.checkForChanges
-      console.log(this.counter)
-    }, 1000);
-
-    localStorage.setItem('events', JSON.stringify(this.events));
-    this.lenEvents = this.events.length;
-    this.selectedEvent = Number(localStorage.getItem('selectedEvent'));
-  }
-  checkForChanges(): void {
-    // Retrieve the current value of counter from localStorage
-    const storedCounter = parseInt(localStorage.getItem('counter') || '0', 10);
-
-    // Compare the current value with the stored value
-    if (this.counter !== storedCounter) {
-      // Update the local variable if there's a change
-      this.counter = storedCounter;
-      console.log('Counter changed:', this.counter);
-      // You can perform further actions based on the changed value here
-    }
-  }
+ 
   ngOnDestroy() {
     clearInterval(this.timer);
   }
+
+  highlightLine(index: number): void {
+    // Set the index of the first dot clicked
+    this.blueDotIndex = this.blueDotIndex !== index ? index : null;
+  }
+
+  isBlueDot(index: number): boolean {
+    // Determine if a dot should be blue
+    return this.blueDotIndex !== null && index <= this.blueDotIndex;
+  }
+  
 }
